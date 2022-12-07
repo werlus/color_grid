af5ae252201782226ab2607bb780b3470be9775f
Justin Saranovic <jsaranovic@gmail.com>
2022-12-07 14:48:46 -0500

    Added Standard and Commander life total reset buttons

--- magic/magicScript.js
+++ magic/magicScript.js
@@ -2,9 +2,13 @@ let lifeTotal = 0
 let lifeBox = document.getElementById("life-total")
 
 function changeLifeTotal(value) {
-    console.log(value)
     let lifeChange = value
-    console.log(value)
     lifeTotal = lifeTotal + lifeChange
+    lifeBox.innerText = lifeTotal
+}
+
+function resetLifeTotal(value) {
+    let lifeChange = value
+    lifeTotal = lifeChange
     lifeBox.innerText = lifeTotal
 }
