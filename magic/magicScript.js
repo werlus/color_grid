464cb5116d2a3170e4e95a65f7636e8b2a368b27
Justin Saranovic <jsaranovic@gmail.com>
2022-12-15 18:28:19 -0500

    Added function to generate life add/subtract buttons via script

--- magic/magicScript.js
+++ magic/magicScript.js
@@ -1,4 +1,6 @@
 let lifeTotal = 0
+const addValues = [1, 2, 5, 10]
+const subtractValues = [-1, -2, -5, -10]
 let lifeBox = document.getElementById("life-total")
 
 function changeLifeTotal(value) {
@@ -12,3 +14,25 @@ function resetLifeTotal(value) {
     lifeTotal = lifeChange
     lifeBox.innerText = lifeTotal
 }
+
+console.log("hi")
+function addButtons(valueArray){
+    let buttonEl = ""
+    let modifier = ""
+    if (valueArray[0] > 0) {
+        method = "add"
+        modifier = "+"
+    } else {
+        method = "subtract"
+    }
+    for (let i = 0; i < valueArray.length; i++) {
+
+        buttonEl += `
+        <button class="${method}" onclick="changeLifeTotal(${valueArray[i]})">${modifier}${valueArray[i]}</button>
+        `
+    }
+    console.log(buttonEl)
+}
+
+addButtons(addValues)
+addButtons(subtractValues)
